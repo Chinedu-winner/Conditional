1f520fda63dcf7a643015d4a4062136f9db5fd3c
Winner Chinedu <chineduwinnerchibundo@gmail.com>
2024-09-05 11:37:38 +0100

    version commit-m

---
+++ script.js
@@ -0,0 +1,12 @@
+var seriousStudent = 'Favour' 
+
+if(seriousStudent == 'Marium'){
+    console.log('yess');
+}
+
+var bestSchool = 'Lautech'
+
+if (bestSchool =='Covenant University'){
+    console.log('bestSchool');
+    
+}
