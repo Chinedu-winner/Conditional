b2a0879b2964ad9f3aa1cb91424363e355aecb2c
Winner Chinedu <chineduwinnerchibundo@gmail.com>
2024-09-08 16:14:46 +0100

    version commit -m

--- script.js
+++ script.js
@@ -3,10 +3,37 @@ var seriousStudent = 'Favour'
 if(seriousStudent == 'Marium'){
     console.log('yess');
 }
+// Buka la hot 
+var isThereSwallow = false  
+var swallow = 'Iyan' 
+var drink = 'pure  water'
 
-var bestSchool = 'Lautech'
+// Joy sent Joseph errand 
+if(isThereSwallow == true){
+    if(swallow == 'Iyan'){
+        console.log('Buy egusi');       
+    }else if (swallow == 'Amala'){
+        console.log('Ewedu');        
+    }else if (swallow == 'Fufu'){
+        console.log('Efo riro');        
+    }else if(swallow == 'semo'){
+        console.log('Okro');        
+    }else {
+        console.log('agbado sisun');      
+    }
+}else {
+    console.log('Buy indomie, hungry  woman size 10');       
+}
 
-if (bestSchool =='Covenant University'){
-    console.log('bestSchool');
-    
+// Akin bought a piece of land for company
+var isTherethatownedofthehouse = "The owner of company" 
+    if (theLeader = true){
+        if (leader ='president'){           
+    } else if (theLeader= 'the staff'){
+        console.log('vice president')        
+    }else if (theLeader='the secretary'){
+        console.log('the Treasury');        
+    }    
+}else {
+    console.log('the leader of the company is the CEO');
 }
